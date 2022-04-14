f0182ea483dee3ffe765523a1b9d926a91dfe630
Rachit Arora <71589920+rcht@users.noreply.github.com>
2022-04-14 09:06:18 +0530

    leap of faith

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -47,50 +47,52 @@ toggleFullscreen :: X ()
 toggleFullscreen = do
     sendMessage ToggleStruts
     withFocused toggleBorder
+    sendMessage ToggleGaps
 
 
 
 
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
-    [ ((modm                , xK_t     ), spawn $ XMonad.terminal conf                       )
-    , ((modm .|. shiftMask  , xK_t     ), spawn "konsole --hide-menubar"                     )
-    , ((modm                , xK_p     ), spawn "LC_CTYPE=en_US.utf8 rofi -show run"         )
-    , ((modm .|. shiftMask  , xK_b     ), spawn "LC_CTYPE=en_US.utf8 rofi-bluetooth"         )
-    , ((modm .|. shiftMask  , xK_p     ), spawn "rofi-pdf"                                   )
-    , ((modm                , xK_q     ), kill                                               )
-    , ((modm                , xK_space ), sendMessage NextLayout                             )
-    , ((modm .|. controlMask, xK_t     ), toggleFullscreen                                   )
-    , ((modm .|. shiftMask  , xK_space ), setLayout $ XMonad.layoutHook conf                 )
-    , ((modm                , xK_n     ), refresh                                            )
-    , ((modm                , xK_Tab   ), windows W.focusDown                                )
-    , ((modm                , xK_j     ), windows W.focusDown                                )
-    , ((modm                , xK_k     ), windows W.focusUp                                  )
-    , ((modm                , xK_m     ), windows W.focusMaster                              )
-    , ((modm                , xK_Return), windows W.swapMaster                               )
-    , ((modm .|. shiftMask  , xK_j     ), windows W.swapDown                                 )
-    , ((modm .|. shiftMask  , xK_k     ), windows W.swapUp                                   )
-    , ((modm                , xK_h     ), sendMessage Shrink                                 )
-    , ((modm                , xK_l     ), sendMessage Expand                                 )
-    , ((modm .|. shiftMask  , xK_Return), withFocused $ windows . W.sink                     )
-    , ((modm                , xK_comma ), sendMessage (IncMasterN 1)                         )
-    , ((modm                , xK_period), sendMessage (IncMasterN (-1))                      )
-    , ((modm                , xK_b     ), spawn "firefox &"                                  )
-    , ((modm .|. shiftMask  , xK_q     ), io exitSuccess                                     )
-    , ((modm .|. shiftMask  , xK_h     ), spawn "xmonad --recompile; xmonad --restart"       )
-    , ((modm .|. shiftMask  , xK_g     ), sendMessage ToggleGaps                             )
-    , ((modm                , xK_a     ), spawn "xmonad-keys-gui"                            )
-    , ((modm .|. shiftMask  , xK_c     ), spawn "flameshot gui"                              )
-    , ((modm                , xK_c     ), spawn "XDG_CURRENT_DESKTOP=KDE cantata"            )
-    , ((modm .|. shiftMask  , xK_a     ), spawn "killall flameshot"                          )
-    , ((modm .|. controlMask, xK_plus  ), sendMessage MagnifyMore                            )
-    , ((modm .|. controlMask, xK_minus ), sendMessage MagnifyLess                            )
-    , ((modm .|. controlMask, xK_m     ), sendMessage Toggle                                 )
-    , ((modm .|. shiftMask  , xK_Right ), shiftToNext                                        )
-    , ((modm .|. shiftMask  , xK_Left  ), shiftToPrev                                        )
-    , ((modm                , xK_g     ), spawn "GalaxyBudsClient_Linux_64bit_Portable.bin &")
-    , ((0                   , xK_F2    ), spawn "amixer -q sset Master 3%-"                  )
-    , ((0                   , xK_F3    ), spawn "amixer -q sset Master 3%+"                  )
-    , ((0                   , xK_F1    ), spawn "amixer -D pulse set Master 1+ toggle"       )
+    [ ((modm                , xK_t     ), spawn $ XMonad.terminal conf                              )
+    , ((modm .|. shiftMask  , xK_t     ), spawn "konsole --hide-menubar"                            )
+    , ((modm                , xK_p     ), spawn "LC_CTYPE=en_US.utf8 rofi -show run"                )
+    , ((modm .|. shiftMask  , xK_b     ), spawn "LC_CTYPE=en_US.utf8 rofi-bluetooth"                )
+    , ((modm .|. shiftMask  , xK_p     ), spawn "rofi-pdf"                                          )
+    , ((modm                , xK_q     ), kill                                                      )
+    , ((modm                , xK_space ), sendMessage NextLayout                                    )
+    , ((modm .|. controlMask, xK_t     ), toggleFullscreen                                          )
+    , ((modm .|. shiftMask  , xK_space ), setLayout $ XMonad.layoutHook conf                        )
+    , ((modm                , xK_n     ), refresh                                                   )
+    , ((modm                , xK_Tab   ), windows W.focusDown                                       )
+    , ((modm                , xK_j     ), windows W.focusDown                                       )
+    , ((modm                , xK_k     ), windows W.focusUp                                         )
+    , ((modm                , xK_m     ), windows W.focusMaster                                     )
+    , ((modm .|. shiftMask  , xK_m     ), spawn $ myTerminal ++ " -e cmus"                          )
+    , ((modm                , xK_Return), windows W.swapMaster                                      )
+    , ((modm .|. shiftMask  , xK_j     ), windows W.swapDown                                        )
+    , ((modm .|. shiftMask  , xK_k     ), windows W.swapUp                                          )
+    , ((modm                , xK_h     ), sendMessage Shrink                                        )
+    , ((modm                , xK_l     ), sendMessage Expand                                        )
+    , ((modm .|. shiftMask  , xK_Return), withFocused $ windows . W.sink                            )
+    , ((modm                , xK_comma ), sendMessage (IncMasterN 1)                                )
+    , ((modm                , xK_period), sendMessage (IncMasterN (-1))                             )
+    , ((modm                , xK_b     ), spawn "firefox &"                                         )
+    , ((modm .|. shiftMask  , xK_q     ), io exitSuccess                                            )
+    , ((modm .|. shiftMask  , xK_h     ), spawn "xmonad --recompile; xmonad --restart"              )
+    , ((modm .|. shiftMask  , xK_g     ), sendMessage ToggleGaps                                    )
+    , ((modm                , xK_a     ), spawn "xmonad-keys-gui"                                   )
+    , ((modm .|. shiftMask  , xK_c     ), spawn "flameshot gui"                                     )
+    , ((modm                , xK_c     ), spawn "rofi -show calc -modi calc -no-show-match -no-sort")
+    , ((modm .|. shiftMask  , xK_a     ), spawn "killall flameshot"                                 )
+    , ((modm .|. controlMask, xK_plus  ), sendMessage MagnifyMore                                   )
+    , ((modm .|. controlMask, xK_minus ), sendMessage MagnifyLess                                   )
+    , ((modm .|. controlMask, xK_m     ), sendMessage Toggle                                        )
+    , ((modm .|. shiftMask  , xK_Right ), shiftToNext                                               )
+    , ((modm .|. shiftMask  , xK_Left  ), shiftToPrev                                               )
+    , ((modm                , xK_g     ), spawn "GalaxyBudsClient_Linux_64bit_Portable.bin &"       )
+    , ((0                   , xK_F2    ), spawn "amixer -q sset Master 3%-"                         )
+    , ((0                   , xK_F3    ), spawn "amixer -q sset Master 3%+"                         )
+    , ((0                   , xK_F1    ), spawn "amixer -D pulse set Master 1+ toggle"              )
     ]
     ++
     [((m .|. modm, k), windows $ f i)
@@ -140,6 +142,8 @@ mySWNConfig = def{
 
 myLayout =  magnifierOff 
          $  showWName' mySWNConfig  
+         -- $  spacingRaw True (Border 0 5 5 5) True (Border 5 5 5 5) True
+         -- $  gaps [(U,30),(R,7),(L,7),(D,7)]
          $  avoidStruts myDefaultLayout
          where
             myDefaultLayout = tiled 
